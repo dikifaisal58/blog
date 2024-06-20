9bc70cf192fbef5facef778dd27d2904a8530c68
dikifaisal58 <dikijonnn@gmail.com>
2024-06-20 23:27:00 +0700

    Delete blog directory

--- blog/includes/footer.php
+++
@@ -1,9 +0,0 @@
-</main>
-    </div> <!-- Closing tag for main container -->
-    <footer>
-        <div class="container">
-            <p>&copy; <?php echo date("Y"); ?> BOOK DFH. All rights reserved.</p>
-        </div>
-    </footer>
-</body>
-</html>
